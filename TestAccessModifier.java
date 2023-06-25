f40bc742cc8a079d3788f6bac2a34058e9358e27
PrashantiWaware <137686962+PrashantiWaware@users.noreply.github.com>
2023-06-25 17:10:23 +0530

    Add files via upload

---
+++ TestAccessModifier.java
@@ -0,0 +1,13 @@
+package MarchThirty_EncapsulationTest;
+
+import MarchThirty_ENCAPSULATION.AccessModifier;
+
+public class TestAccessModifier {
+
+	public static void main(String[] args) {
+		AccessModifier am=new AccessModifier();
+		System.out.println(am.dft);
+
+	}
+
+}
