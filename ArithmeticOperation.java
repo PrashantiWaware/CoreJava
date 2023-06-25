01a279181b46dd5545292a19c9b733f68aa856f8
PrashantiWaware <137686962+PrashantiWaware@users.noreply.github.com>
2023-06-25 16:56:23 +0530

    Add files via upload

---
+++ ArithmeticOperation.java
@@ -0,0 +1,17 @@
+package SeventeenMarchIntro;
+
+public class ArithmeticOperation {
+
+	public static void main(String[] args) {
+		int a=10,b=6;
+		int sum=a+b;
+		
+		float avg=(float) sum/2;
+		
+		System.out.println(" Sum is>> "+sum);
+		System.out.println("AVG is>> "+avg);
+		
+
+	}
+
+}
---
+++ Naming.java
@@ -0,0 +1,13 @@
+package SeventeenMarchIntro;
+
+public class Naming {
+
+	public static void main(String[] args) {
+		byte             a                  =                 10;
+      //data_type       variable_name     Operator          value/data
+		
+		System.out.println("hello");
+
+	}
+
+}
